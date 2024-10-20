d8605da5e5acebd0a65def53f9081c63b08fabd0
Grygoryy <kachurovskyy123@gmail.com>
2024-10-20 21:39:56 +0300

    speed insights added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,20 +1,21 @@
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import { Play } from "next/font/google"
-import "./globals.css";
+import "./globals.css"
 
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import Footer from "../components/Footer"
+import Header from "../components/Header"
 
 const play = Play({
   subsets: ["latin"],
   weight: ["400", "700"],
   variable: "--font-play"
-});
+})
 
 export const metadata = {
   title: "GameAtlas",
   description: "Where Every Game Tells a Story",
-};
+}
 
 export default function RootLayout({ children }) {
   return (
@@ -26,7 +27,8 @@ export default function RootLayout({ children }) {
           <Footer />
         </div>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
-  );
+  )
 }
